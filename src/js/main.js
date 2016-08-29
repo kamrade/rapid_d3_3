7282f9f3e0f19799346e73b229232c9a057cc378
Denis Michailov <kamrad.reg@gmail.com>
2016-08-29 21:38:28 +1000

    Take the keys from the file

--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,19 @@ console.log( "d3 version is " + d3.version );
 
 // VARS ********************************
 var positions = {G:"Goalkeeper", D:"Defender", M:"Midfielder", F:"Forward"};
-var columns = ["No", "Name", "Pos"];
 
 window.data = [];
+var teams = [];
 var table = d3.select(".content")
     .append("table")
     .classed("table", true);
-var thead = table.append("thead");
+var thead = table.append("thead").append("tr");
 var tbody = table.append("tbody");
-
 
 
 // RELOAD ********************************
 var reload = function(){
-    d3.tsv('/data/afcw-roster.tsv', function(rows){
+    d3.tsv('/data/eng2-rosters.tsv', function(rows){
         data = rows;
         data.forEach(function(d){
             d.Pos = positions[d.Pos];
@@ -31,34 +30,31 @@ var reload = function(){
 
 // REDRAW ********************************
 var redraw = function(){
-
-    thead.selectAll("tr")
-        .data([columns])
+    thead.selectAll("th")
+        .data(d3.map(data[0]).keys())
         .enter()
-        .append("tr")
-            .selectAll("th")
-            .data(function(d) { return d; })
-            .enter()
-            .append("th")
-            .text(function(d) { return d; })
+        .append("th")
+        .text(function(d){ return d; })
     ;
 
     var rows = tbody.selectAll("tr")
         .data(data);
+    
     rows.enter().append("tr");
-    // если мы уже загрузили объект
-    // и хотим загрузить другой, то если он короче, удаляет оставшиеся <tr>
     rows.exit().remove();
+
+    var h = d3.map(data[0]).keys()
 
     var cells = rows.selectAll("td")
         .data(function(row){
-            return columns.map(function(col){
+            return h.map(function(col){
                 return row[col];
             });
         });
 
     cells.enter().append("td");
     cells.text(function(d){ return d; });
+
 };
 
 reload();
