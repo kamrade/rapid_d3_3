3133e0adcfef6b2fa8881b91f095746cb91bad00
Denis Michailov <denis.michailov@gmail.com>
2016-09-13 11:43:41 +1000

    colored map. co2 emmiting range

--- src/js/main.js
+++ src/js/main.js
@@ -6,28 +6,90 @@ var classie = require("./classie");
 var w = 1024;
 var h = 768;
 
+var year = 1999;
+
+var container = document.querySelector(".content");
+console.dir(container);
+console.log(container.clientWidth);
+console.log(container.clientHeight);
+
+// w = container.clientWidth;
+// h = container.clientHeight;
+
+window.addEventListener("resize", function(){
+	console.log("resize");
+});
+
+// https://github.com/d3/d3-3.x-api-reference/blob/master/Geo-Projections.md
+// http://bl.ocks.org/mbostock/3757132
 var projection = d3.geo.mercator()
 						.center([0, 40])
 						.translate([ w/2, h/2 ])
 						.scale( [w/8] );
 
+// В projection мы кладем [Longitude, Latitude]
+//  для работы с консолью делаем его глобальным
+window.projection = projection;
+window.d3 = d3;
 
+// определяем path-generator
 var path = d3.geo.path().projection(projection);
 
+var color = d3.scale.quantize()
+					.range([ "#ffffb2", "#fecc5c", "#fd8d3c", "#f03b20", "#bd0025" ]);
 
-var svg = d3.select("body")
+// создаем svg
+var svg = d3.select(".content")
 				.append("svg")
 				.attr("width", w)
 				.attr("height", h);
 
-d3.json("/data/geo02/ne_50m_admin_0_countries.json", function(json){
-	var data = json.features;
+d3.csv("/data/co2_emissions.csv", function(data){
+	color.domain([
+		d3.min(data, function(d){ return +d[year]; }),
+		d3.max(data, function(d){ return +d[year]; })
+	]);
 
-	svg.selectAll("path")
-				.data(data)
-				.enter()
-				.append("path")
-				.classed("country", true)
-				.attr("d", path);
+	console.log(data);
 
-});
+
+	d3.json("/data/geo02/ne_50m_admin_0_countries.json", function(json){
+		
+		for(var i = 0; i < data.length; i++) {
+			// grab country name
+			var dataCountryCode = data[i].countryCode
+
+			var dataValue = +data[i][year];
+
+			for(var j = 0; j < json.features.length; j++) {
+				var jsonCountryCode = json.features[j].properties.iso_a3;
+				
+				if(dataCountryCode == jsonCountryCode) {
+					json.features[j].properties.co2 = dataValue;
+					break;
+				}
+
+			}
+		}
+
+		svg.selectAll("path")
+					.data(json.features)
+					.enter()
+					.append("path")
+					.classed("country", true)
+					.attr("d", path)
+					.style("fill", function(d){
+						var value = d.properties.co2;
+						if(value) {
+							return color(value);
+						} else {
+							return "#333";
+						}
+					})
+
+	});
+
+
+})
+
+
